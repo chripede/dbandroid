19275a530590dbb5387181d9755e84bdcd1b66ab
Christian <chripede@gmail.com>
2010-12-17 11:43:15 +0100

    removed scan button to simplify presentation source code

--- src/dk/dba/android/MainSearch.java
+++ src/dk/dba/android/MainSearch.java
@@ -1,33 +1,18 @@
 package dk.dba.android;
 
-import java.util.HashMap;
-import java.util.Map;
-
-import org.xmlrpc.android.XMLRPCClient;
-import org.xmlrpc.android.XMLRPCException;
-
 import android.app.Activity;
-import android.app.ProgressDialog;
-import android.content.Intent;
-import android.os.AsyncTask;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.View.OnKeyListener;
 import android.widget.Button;
 import android.widget.EditText;
-
-import com.google.zxing.integration.android.IntentIntegrator;
-import com.google.zxing.integration.android.IntentResult;
-
 import dk.dba.android.util.UIUtils;
 
 public class MainSearch extends Activity {
 
 	private Button searchButton;
-	private Button scanButton;
 	private EditText searchText;
 
 	@Override
@@ -41,7 +26,6 @@ public class MainSearch extends Activity {
 
 	private void findAllViewsById() {
 		searchButton = (Button) findViewById(R.id.searchButton);
-		scanButton = (Button) findViewById(R.id.scanButton);
 		searchText = (EditText) findViewById(R.id.searchText);
 	}
 
@@ -62,63 +46,5 @@ public class MainSearch extends Activity {
 				UIUtils.doSearch(MainSearch.this, searchText.getText());
 			}
 		});
-		
-		scanButton.setOnClickListener(new OnClickListener() {
-			public void onClick(View v) {
-				IntentIntegrator.initiateScan(MainSearch.this);
-			}
-		});
-	}
-
-	public void onActivityResult(int requestCode, int resultCode, Intent intent) {
-		IntentResult scanResult = IntentIntegrator.parseActivityResult(requestCode, resultCode, intent);
-		if (scanResult.getContents() != null) {
-			new UpcDatabaseTask(scanResult).execute();
-		}
-	}
-	
-	private class UpcDatabaseTask extends AsyncTask<Void, Void, Void> {
-
-		private ProgressDialog dialog;
-		private IntentResult scanResult;
-		
-		public UpcDatabaseTask(IntentResult result) {
-			scanResult = result;
-		}
-		
-		@Override
-		protected void onPostExecute(Void result) {
-			dialog.dismiss();
-		}
-
-		@Override
-		protected void onPreExecute() {
-			dialog = ProgressDialog.show(MainSearch.this, "Henter data", "Vent venligst..."); 
-		}
-
-		@SuppressWarnings("unchecked")
-		@Override
-		protected Void doInBackground(Void... params) {
-			XMLRPCClient client = new XMLRPCClient("http://www.upcdatabase.com/xmlrpc");
-			try {
-				Map<String, String> rpcParams = new HashMap<String, String>();
-				rpcParams.put("rpc_key", "cf25e34e3d92db42079e83f1b11e6b43f88bcfcf");
-				rpcParams.put("upc", scanResult.getContents());
-				HashMap<String, String> result = (HashMap<String, String>) client.call("lookup", rpcParams);
-				//String resultSize = result.get("size");
-				final String resultDesc = result.get("description");
-				MainSearch.this.runOnUiThread(new Runnable() {
-					public void run() {
-						searchText.setText(resultDesc);
-					}
-				});
-			} catch (NullPointerException e) {
-				Log.e("onActivityResult:NullPointerException", e.getMessage());
-			} catch (XMLRPCException e) {
-				Log.e("onActivityResult:XMLRPCException", e.getMessage());
-			}
-			return null;
-		}
-		
 	}
 }
